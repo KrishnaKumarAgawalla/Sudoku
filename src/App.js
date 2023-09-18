31a8901de450365e3d537cba77117a149a6ce5ef
Krishna Kumar Agrawalla <krishnakumaragrawalla@gmail.com>
2023-09-18 13:43:47 +0530

    Board Layout and Numpad

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./index.css";
+import "./app.css";
 import Board from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>Sudoku</h1>
+      <h1 className="app-heading">Sudoku</h1>
       <Board/>
     </div>
   );
--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -2,29 +2,43 @@ import React, { useState } from "react";
 import "./Board.css";
 
 const Board = () => {
-  const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  const [board, setBoard] = useState(Array.from({length: 9}, () => Array.from({length: 9}).fill(0)));
+
+  const selectionNum = Array.from({ length: 9 }, (_, index) => index + 1);
 
   return (
     <>
       <div className="board">
         {board.map((row, rIndex) => (
-          <div className="row" key={rIndex}>
+          <div
+            className="row"
+            key={rIndex}
+            style={
+              rIndex < row.length - 1 && (rIndex + 1) % 3 === 0
+                ? { borderBottom: "5px solid black" }
+                : {}
+            }
+          >
             {row.map((value, cIndex) => (
-              <div className="col app-flex" key={cIndex}>
+              <div
+                className="col app-flex"
+                key={cIndex}
+                style={
+                  cIndex < row.length - 1 && (cIndex + 1) % 3 === 0
+                    ? { borderRight: "5px solid black" }
+                    : {}
+                }
+              >
                 {value}
               </div>
             ))}
           </div>
+        ))}
+      </div>
+
+      <div className="board-num ">
+        {selectionNum.map((num, index) => (
+          <div className="board-num-selection app-flex" key={index + 1}>{num}</div>
         ))}
       </div>
     </>
